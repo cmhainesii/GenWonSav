4011fb8ed7d4d39d4e8dfcdcf74cb69dcd1b7800
Charles Haines <crono102@gmail.com>
2024-01-20 00:29:48 -0500

    badges refactor
    changed iternal data structure of badges from bools to an array of bools

--- GenWonSav/objects/Data/Pokemon/Badges.cs
+++ GenWonSav/objects/Data/Pokemon/Badges.cs
@@ -1,67 +1,89 @@
+using System.Security;
 using System.Text;
 
 public class Badges
 {
-    public bool boulder { get; set; }
-    public bool cascade { get; set; }
-    public bool thunder { get; set; }
-    public bool rainbow { get; set; }
-    public bool soul { get; set; }
-    public bool marsh { get; set; }
-    public bool volcano { get; set; }
-    public bool earth { get; set; }
+    private bool[] badges;
 
     public Badges(bool boulder, bool cascade, bool thunder, bool rainbow,
         bool soul, bool marsh, bool volcano, bool earth)
     {
-        this.boulder = boulder;
-        this.cascade = cascade;
-        this.thunder = thunder;
-        this.rainbow = rainbow;
-        this.soul = soul;
-        this.marsh = marsh;
-        this.volcano = volcano;
-        this.earth = earth;
+        badges = new bool[8];
+
+        this.badges[0] = boulder;
+        this.badges[1] = cascade;
+        this.badges[2] = thunder;
+        this.badges[3] = rainbow;
+        this.badges[4] = soul;
+        this.badges[5] = marsh;
+        this.badges[6] = volcano;
+        this.badges[7] = earth;
+    }
+
+    public Badges()
+    {
+        badges = new bool[8];
     }
 
     public string getBadgesInfo()
     {
         StringBuilder sb = new StringBuilder();
 
-        if(boulder)
+        sb.AppendLine("Badge Status:");
+        sb.AppendLine();
+        sb.AppendLine($"Obtained {GetNumBadges():D1}/8 badges");
+        sb.AppendLine();
+        sb.AppendLine("Badges Obtained:");
+
+        if(badges[0])
         {
             sb.AppendLine("Boulder Badge");
         }
-        if (cascade)
+        if (badges[1])
         {
             sb.AppendLine("Cascade Badge");
         }
-        if (thunder)
+        if (badges[2])
         {
             sb.AppendLine("Thunder Badge");
         }
-        if (rainbow)
+        if (badges[3])
         {
             sb.AppendLine("Rainbow Badge");
         }
-        if (soul)
+        if (badges[4])
         {
             sb.AppendLine("Soul Badge");
         }
-        if (marsh)
+        if (badges[5])
         {
             sb.AppendLine("Marsh Badge");
         }
-        if (volcano)
+        if (badges[6])
         {
             sb.AppendLine("Volcano Badge");
         }
-        if (earth)
+        if (badges[7])
         {
             sb.AppendLine("Earth Badge");
         }
 
         return sb.ToString();
+    }
+
+    public ushort GetNumBadges()
+    {
+        ushort sum = 0;
+
+        foreach (bool badge in badges)
+        {
+            if (badge)
+            {
+                sum++;
+            }
+        }
+
+        return sum;
     }
 
 }
